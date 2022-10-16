a87b2231ea718dd8f43268f52334b1c59d94d0d6
lsilvamatheus <lsilva.matheus@gmail.com>
2022-10-16 10:17:05 -0300

    Adicionando classe RotateMatrixApplication.java

---
+++ src/main/java/com/matheus/leite/rotatematrix/RotateMatrixApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.leite.rotatematrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotateMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotateMatrixApplication.class, args);
+	}
+
+}
