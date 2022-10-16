9442e875cf7bd2778d3f21ebf4f52bbf3df28258
lsilvamatheus <lsilva.matheus@gmail.com>
2022-10-16 10:32:16 -0300

    Adicionando classe ErrorMessage

---
+++ src/main/java/com/matheus/leite/rotatematrix/error/message/ErrorMessage.java
@@ -0,0 +1,8 @@
+package com.matheus.leite.rotatematrix.error.message;
+
+public class ErrorMessage {
+    private ErrorMessage(){}
+    
+    public static final String UNEXPECTED_ERROR = "An unexpected error has occurred.";
+    public static final String JSON_PARSE_ERROR = "JSON parse error: Cannot deserialize input value. Please check your request body.";
+}
